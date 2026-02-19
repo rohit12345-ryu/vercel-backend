ea8cfe679bc60bd4d31bb419d709ef21459b100d
rohit12345-ryu <ry46280206@gmail.com>
2026-02-19 20:23:16 +0530

    fixed vercel config

--- server.js
+++ server.js
@@ -54,6 +54,4 @@ mongoose
   .catch((err) => console.error("MongoDB error:", err));
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-;
+module.exports = app;
