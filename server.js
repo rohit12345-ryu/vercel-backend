72ec5133df37dd658b2f5af44884a0f3edf053d5
rohit12345-ryu <ry46280206@gmail.com>
2026-02-19 20:33:47 +0530

    fixed mongoose serverless connection

--- server.js
+++ server.js
@@ -49,9 +49,21 @@ app.get("/api/protected", protect, (req, res) => {
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB error:", err));
+  let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) return;
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    isConnected = true;
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+};
+
+connectDB();
 
 
 module.exports = app;
